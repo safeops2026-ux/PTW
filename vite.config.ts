c6a1758e20fa3aabaf163eedbda15ccbba2c53bb
safeops2026-ux <safeops2025@gmail.com>
2026-07-06 23:01:18 +0530

    feat: Configure deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/PTW/', // Set the base path for GitHub Pages
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    // Use a specific base path for the default build (for GitHub Pages),
+    // and the root path for all other modes (like production for Firebase).
+    base: mode === 'production' ? '/' : '/PTW/',
+    plugins: [react()],
+  }
 })
